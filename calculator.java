014dc70b459fc5154f210b54204e9c2d3b5c0b93
raunak29.com <raunak@git.com>
2025-04-09 13:00:23 +0530

    Indentation changes

--- calculator.java
+++ calculator.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter first number");
+        System.out.println("Enter first number : ");
         int a = sc.nextInt();
-        System.out.println("Enter second number");
+        System.out.println("Enter second number : ");
         int b = sc.nextInt();
-        System.out.println("Enter operator");
+        System.out.println("Enter operator : ");
         char c = sc.next().charAt(0);
         if (c == '+')
             System.out.println(a + b);
