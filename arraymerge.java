5331c7112f8caad979b03fa2e4b3b39d1fc25ddd
raunak29.com <raunak@git.com>
2025-04-22 16:03:35 +0530

    java training day 8

--- arraymerge.java
+++ arraymerge.java
@@ -1,22 +1,17 @@
 public class arraymerge {
 
-        public static void main(String[] args) {
-            int[] a1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            int[] a2 = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            int[] a3 = new int[a1.length +a2.length];
-for(int i=0,j=0;i<a3.length;i++,j++)
-{
-    if(j<a1.length)
-{
-    a3[i]=a1[j];
-
-}
-else 
-a3[i]=a2[j];
-
-}
-for (int i = 0; i < a3.length; i++) {
-    System.out.print(a3[i] + "\t");
-}
-}
+    public static void main(String[] args) {
+        int[] a1 = { 1, 2, 3, 4, 5, 6, 7, 8, 1 };
+        int[] a2 = { 23, 24, 25, 26, 27, 28, 29, 30 };
+        int[] a3 = new int[a1.length + a2.length];
+        for (int i = 0; i < a1.length; i++) {
+            a3[i] = a1[i];
+        }
+        for (int i = 0; i < a2.length; i++) {
+            a3[a1.length+i] = a2[i];
+        }
+        for (int i = 0; i < a3.length; i++) {
+            System.out.print(a3[i] + "\t");
+        }
+    }
 }
---
+++ stringprint.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class stringprint {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String str = sc.nextLine();
+        String str2 = "";
+        System.out.println(str);
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            str2 += str.charAt(i);
+
+        }
+        System.out.println(str2);
+        if (str.equals(str2)) {
+            System.out.println("palindrome");
+        } else {
+            System.out.println("not palindrome");
+        }
+        sc.close();
+    }
+}
