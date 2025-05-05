e3685ac4acaae18e65d8ae3a4ec112b565b78836
raunak29.com <raunak@git.com>
2025-05-05 21:50:09 +0530

    overloading & overriding

---
+++ objectoriented/areaoverload.java
@@ -0,0 +1,24 @@
+public class areaoverload {
+    public static void main(String[] args) {
+        area obj = new area();
+        System.out.println("Area of Square is " + obj.calculatearea(15));
+        System.out.println("Area of Rectangle is " + obj.calculatearea(15, 10));
+        System.out.println("Area of Triangle is " + obj.calculatearea(15.0, 12.0));
+
+    }
+}
+
+class area {
+    int calculatearea(int l, int b) { // for rectangle
+        return l * b;
+    }
+
+    int calculatearea(int s) { // for square
+        return s * s;
+    }
+
+    double calculatearea(double b, double h) { // for triangle
+        return 0.5 * b * h;
+    }
+
+}
